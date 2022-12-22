9ddc67557b9931e0f85c000d1f4abf560714ae4b
DouglasB834 <douglas_borges09@hotmail.com>
2022-12-21 22:56:28 -0300

    feat/clean code

--- src/Services/schedules/listSchedules.Service.ts
+++ src/Services/schedules/listSchedules.Service.ts
@@ -29,17 +29,6 @@ export const listSchedulesService =async (idProperty:String|any)=> {
     .innerJoinAndSelect("schedules.user", "userId")
     .innerJoinAndSelect("properties.category", "categoryId")
     .getOne();
-
-    
-// SELECT
-// p.id , p.sold, p.value ,p."size" , p."createdAt" , p."updatedAt", sup.id , sup."date",
-// sup."hour", u.*, a.*, c.*
-// FROM Properties p
-// JOIN "schedulesUserProperties" sup ON  sup."propertyId"  = p.id
-// JOIN addresses a ON a.id = p."addressId" 
-// JOIN users u ON u.id = sup."userId" 
-// JOIN  categories c ON c.id = p."categoryId" 
-// WHERE p.id = '031a589b-2133-4d62-aeac-39fd0cd0f516'
    
     return selectProperty
 
